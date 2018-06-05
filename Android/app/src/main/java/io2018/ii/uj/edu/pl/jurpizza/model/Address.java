fdefe481e326b77d2f2b33704f72e4d873c0d9d5
Grzegorz Łazarski <golui889@gmail.com>
2018-06-05 12:56:11 +0200

    Address & Beverage

---
+++ Android/app/src/main/java/io2018/ii/uj/edu/pl/jurpizza/model/Address.java
@@ -0,0 +1,73 @@
+package io2018.ii.uj.edu.pl.jurpizza.model;
+
+import java.util.regex.Pattern;
+
+import io2018.ii.uj.edu.pl.jurpizza.exception.AddressFormatException;
+
+public class Address {
+
+    public static final String[] ALLOWED_TOWNS = {"Kraków", "Warszawa", "Helsinki"};
+
+    public static final Pattern POSTAL_CODE_PATTERN = Pattern.compile("\\d\\d-\\d\\d\\d");
+    // Leaving in case we change input
+    // public static final Pattern FLAT_NUMBER_PATTERN = Pattern.compile("[a-zA-Z0-9]*/[a-zA-Z0-9]");
+
+    int town;
+    String postalCode;
+    String street;
+    String house;
+    String flat;
+
+    public void setTown(int t)
+    {
+        this.town = t;
+    }
+
+    public void setPostalCode(String raw) throws AddressFormatException
+    {
+        if(POSTAL_CODE_PATTERN.matcher(raw).matches())
+        {
+            this.postalCode = raw;
+        }else
+        {
+            throw new AddressFormatException("Postal code is not a postal code");
+        }
+    }
+
+    public void setStreet(String str)
+    {
+        this.street = str;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    public void setFlat(String flat) {
+        this.flat = flat;
+    }
+
+    public int getTown() {
+        return town;
+    }
+
+    public String getTownString() {
+        return ALLOWED_TOWNS[town];
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getFlat() {
+        return flat;
+    }
+}
--- Android/app/src/main/java/io2018/ii/uj/edu/pl/jurpizza/model/Beverage.java
+++ Android/app/src/main/java/io2018/ii/uj/edu/pl/jurpizza/model/Beverage.java
@@ -1,19 +1,73 @@
 package io2018.ii.uj.edu.pl.jurpizza.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Beverage implements BasketEntry {
+
+    public Beverage(String s, Variant... variants)
+    {
+        this.baseName = s;
+        this.variants = new ArrayList<>(Arrays.asList(variants));
+    }
+
+    public Beverage(Beverage b)
+    {
+        this.variants = new ArrayList<>(b.variants);
+        Collections.sort(this.variants);
+        this.baseName = b.baseName;
+        this.variant = b.variant;
+    }
+
+    public static class Variant implements Comparable<Variant>
+    {
+        String volume;
+        int price;
+
+        public Variant(String v, int p)
+        {
+            this.volume = v;
+            this.price = p;
+        }
+
+        @Override
+        public int compareTo(Variant o2) {
+            return Integer.compare(this.price, o2.price);
+        }
+    }
+
+    private String baseName;
+    private List<Variant> variants;
+    private int variant = -1;
 
     @Override
     public String getName() {
-        return "DUMMY";
+        return baseName;
     }
 
     @Override
+    /**
+     * Returns lowest price, to entice clients
+     */
     public int getPrice() {
-        return 0;
+        return this.variants.get(this.variant == -1 ? 0 : this.variant).price;
+    }
+
+    public List<Variant> getAvailableVolumes() { return this.variants; }
+
+    public void setVariants(List<Variant> var) {
+        this.variants = var;
+    }
+
+    public void setVariant(int variant) {
+        this.variant = variant;
     }
 
     @Override
     public String getBasketString() {
-        return "DUMMY";
+        return this.baseName + (this.variant != -1 ? " (" + this.variants.get(this.variant).volume + ")" : "");
     }
 }
